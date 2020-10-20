7ee9b2d35bb2feded50a8ffbc73e6d2de566e43d
Pramod Kumar <50309120+kspramod@users.noreply.github.com>
2020-10-20 15:06:14 +1100

    Use fabric package and design to integrate with fabric

--- client.js
+++ client.js
@@ -1,35 +1,55 @@
-const { HelloRequest, RequestMessage } = require("./hellogreeter_pb");
-const { GreeterClient } = require("./hellogreeter_grpc_web_pb");
+const {
+  GetPartyRequest,
+  GetPartyResponse,
+} = require("@anzx/fabricapis-selfservice/fabric/service/selfservice/v1alpha4/self_service_api_pb");
+const {
+  PartyAPIClient,
+} = require("@anzx/fabricapis-selfservice/fabric/service/selfservice/v1alpha4/self_service_api_grpc_web_pb");
 
-const client = new GreeterClient("http://localhost:8082");
+const client = new PartyAPIClient("https://fabric.gcpnp.anz");
 
-const request = new HelloRequest();
-const requestBody = new RequestMessage();
+const request = new GetPartyRequest();
 
-const getData = () => {
-  requestBody.setOperation("REPLACE");
-  requestBody.setField("name");
-  requestBody.setValue("New Name");
-  request.addRequest(requestBody);
-  client.sayHello(request, {}, (err, response) => {
-    if (err) {
-      const textElement = document.getElementById("error").innerText;
-      document.getElementById("error").innerText =
-        textElement + "\n" + err.message;
-    } else if (response) {
-      const textElement = document.getElementById("output").innerText;
-      document.getElementById("output").innerText =
-        textElement +
-        "\n" +
-        response.getPreferredname() +
-        ", " +
-        response.getSecuritymobile();
+const USERS = {
+  kevin: {
+    name: 'KEVIN WATKINS',
+    userName: 'e9306b6a-adb8-42b4-a705-05ee56e84961',
+    password: 'gyeoZlUm2AE6jBIJWHeDL2p5KdAqjAmL',
+  },
+  wendy: {
+    name: 'WENDY DOBB',
+    userName: '34deae96-72f7-4841-b28b-2a639e0fe1e0',
+    password: '8WOvKoFEqW620ZH087c2dcEuxDsACGdl',
+  }
+}
+
+const getData = ({ name, userName, password }) => {
+  client.getParty(
+    request,
+    {
+      env: "sit",
+      authorization:
+        `Basic ${btoa(`${userName}:${password}`)}`,
+    },
+    (err, response) => {
+      if (err) {
+        const textElement = document.getElementById("error").innerText;
+        document.getElementById("error").innerText =
+          textElement + "\n" + err.message;
+        console.log("Error: ", err);
+      } else {
+        const textElement = document.getElementById("output").innerText;
+        document.getElementById("output").innerText =
+          `${textElement} \n Name: ${name} \n Legal Name: ${response.getLegalName()} \n Residential Address: ${response.getResidentialAddress()} \n Mailing Address: ${response.getMailingAddress()} \n -----------------------------------------`;
+        console.log("Success: ", response.toObject());
+      }
     }
-  });
+  );
 };
 
-getData();
+getData(USERS.kevin);
 
 setTimeout(() => {
-  document.getElementById("try-again").onclick = getData;
+  document.getElementById("try-kevin").onclick = () => {getData(USERS.kevin)};
+  document.getElementById("try-wendy").onclick = () => {getData(USERS.wendy)};
 }, 1000);
