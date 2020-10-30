3af6c65d0222fb07ba13dbc71dfd51ae2075fb7f
Pramod Kumar <50309120+kspramod@users.noreply.github.com>
2020-10-30 18:34:53 +1100

    Update design for different env api calls

--- client.js
+++ client.js
@@ -12,7 +12,49 @@ const {
   AccountAPIClient,
 } = require("@kspramod/fabricapis-selfservice/fabric/service/accounts/v1alpha5/account_api_grpc_web_pb");
 
-const client = new PartyAPIClient("https://fabric.gcpnp.anz");
+const getParams = (pamramKey) => {
+  const urlParams = new URLSearchParams(window.location.search);
+
+  return urlParams.get(pamramKey) || "";
+};
+
+document.cookie = `anzssotoken=${getParams(
+  "token"
+)}; path=/; domain=.gcpnp.anz; secure; samesite=lax`;
+
+let aegisPath = "http://ig-auth-local.default.svc.cluster.local:80";
+updateAegisEnv = (env) => {
+  switch (env) {
+    case "non-prod":
+      aegisPath = "https://identity-services-np-edge.apps.x.gcpnp.anz";
+      break;
+    case "dev":
+      aegisPath = "https://identity-services-dev-edge.apps-dev.x.gcpnp.anz";
+      break;
+    case "devint":
+      aegisPath =
+        "https://devint.identity-services-ldev-edge.apps-dev.x.gcpnp.anz";
+      break;
+    case "feature":
+      aegisPath =
+        "https://abt-12755-fabric.identity-services-ldev-edge.apps-dev.x.gcpnp.anz";
+      break;
+    default:
+      break;
+  }
+};
+
+let envFabric = "sit";
+updateFabricEnv = (envFabric) => {
+  envFabric = envFabric;
+};
+
+let client = new PartyAPIClient(aegisPath, null, {
+  withCredentials: true,
+});
+const clientDirect = new PartyAPIClient("https://fabric.gcpnp.anz");
+const clientAccountsDirect = new AccountAPIClient("https://fabric.gcpnp.anz");
+
 const request = new GetPartyRequest();
 
 const USERS = {
@@ -28,76 +70,96 @@ const USERS = {
   },
 };
 
-const getData = ({ name, userName, password }) => {
-  client.getParty(
-    request,
-    {
-      env: "sit",
-      authorization: `Basic ${btoa(`${userName}:${password}`)}`,
-    },
-    (err, response) => {
-      if (err) {
-        const textElement = document.getElementById("error").innerText;
-        document.getElementById("error").innerText =
-          textElement + "\n" + err.message;
-        console.log("Error: ", err);
-      } else {
-        const textElement = document.getElementById("output").innerText;
-        document.getElementById(
-          "output"
-        ).innerText = `${textElement} \n Name: ${name} \n Legal Name: ${response.getLegalName()} \n Residential Address: ${response.getResidentialAddress()} \n Mailing Address: ${response.getMailingAddress()} \n -----------------------------------------`;
-        console.log("Success: ", response.toObject());
-      }
-    }
-  );
+const handlePartyCallBack = (name, direct) => (err, response) => {
+  const directMessage = `GetParty ${direct ? "DIRECT" : ""} Call \n`;
+
+  if (err) {
+    const textElement = document.getElementById("error").innerText;
+    document.getElementById(
+      "error"
+    ).innerText = `${textElement} \n ${directMessage} \n Name: ${name} \n ${err.message} \n ----------------------------------------- `;
+    console.log("Error: ", err);
+  } else {
+    const textElement = document.getElementById("output").innerText;
+    document.getElementById(
+      "output"
+    ).innerText = `${textElement} \n ${directMessage} \n Name: ${name} \n Legal Name: ${response.getLegalName()} \n Residential Address: ${response.getResidentialAddress()} \n Mailing Address: ${response.getMailingAddress()} \n -----------------------------------------`;
+    console.log("Success: ", response.toObject());
+  }
 };
 
-const clientAccounts = new AccountAPIClient(
-  "https://abt-13800-fabric.identity-services-ldev-edge.apps-dev.x.gcpnp.anz",
-  null,
-  {
-    withCredentials: true,
+const getData = ({ name, userName, password }, direct) => {
+  if (direct) {
+    clientDirect.getParty(
+      request,
+      {
+        env: envFabric,
+        authorization: `Basic ${btoa(`${userName}:${password}`)}`,
+      },
+      handlePartyCallBack(name, direct)
+    );
+  } else {
+    client = new PartyAPIClient(aegisPath, null, {
+      withCredentials: true,
+    });
+    client.getParty(
+      request,
+      {
+        env: envFabric,
+        authorization: `Basic ${btoa(`${userName}:${password}`)}`,
+      },
+      handlePartyCallBack(name)
+    );
   }
-);
+};
+
+let clientAccounts = new AccountAPIClient(aegisPath, null, {
+  withCredentials: true,
+});
 const requestAccount = new GetAccountListRequest();
 
-const getParams = (pamramKey) => {
-  const urlParams = new URLSearchParams(window.location.search);
-
-  return urlParams.get(pamramKey) || "";
+const handleAccountsCallback = (name, direct) => (err, response) => {
+  const directMessage = `Accounts ${direct ? "DIRECT" : ""} Call \n`;
+  if (err) {
+    const textElement = document.getElementById("error").innerText;
+    document.getElementById(
+      "error"
+    ).innerText = `${textElement} \n ${directMessage} \n Name: ${name} \n ${err.message} \n ----------------------------------------- `;
+    console.log("Error: ", err);
+  } else {
+    const textElement = document.getElementById("output").innerText;
+    document.getElementById(
+      "output"
+    ).innerText = `${textElement} \n ${directMessage} \n Name: ${name} \n Accounts List: ${response.getAccountListList()} \n -----------------------------------------`;
+    console.log("Success: ", response.toObject());
+  }
 };
 
-document.cookie = `anzssotoken=${getParams(
-  "token"
-)}; path=/; domain=.gcpnp.anz; secure; samesite=lax`;
-
-const getAccounts = ({ name, userName, password }) => {
-  clientAccounts.getAccountList(
-    requestAccount,
-    {
-      env: "sit",
-      // authorization: `Basic ${btoa(`${userName}:${password}`)}`,
-      // authorization: `Bearer c_Ft3oO2bB3HAlGmxzx1cjUb9c8.*AAJTSQACMDIAAlNLABx1ZDBTcEVGSEt2NlVLYm1Ub1pGaExYRnpBRHc9AAR0eXBlAANDVFMAAlMxAAIwMQ..*`,
-      // Cookie: `anzssotoken=c_Ft3oO2bB3HAlGmxzx1cjUb9c8.*AAJTSQACMDIAAlNLABx1ZDBTcEVGSEt2NlVLYm1Ub1pGaExYRnpBRHc9AAR0eXBlAANDVFMAAlMxAAIwMQ..*`,
-    },
-    (err, response) => {
-      if (err) {
-        const textElement = document.getElementById("error").innerText;
-        document.getElementById("error").innerText =
-          textElement + "\n" + err.message;
-        console.log("Error: ", err);
-      } else {
-        const textElement = document.getElementById("output").innerText;
-        document.getElementById(
-          "output"
-        ).innerText = `${textElement} \n Name: ${name} \n Accounts List: ${response.getAccountListList()} \n -----------------------------------------`;
-        console.log("Success: ", response.toObject());
-      }
-    }
-  );
+const getAccounts = ({ name, userName, password }, direct) => {
+  if (direct) {
+    clientAccountsDirect.getAccountList(
+      requestAccount,
+      {
+        env: envFabric,
+        authorization: `Basic ${btoa(`${userName}:${password}`)}`,
+      },
+      handleAccountsCallback(name, direct)
+    );
+  } else {
+    clientAccounts = new AccountAPIClient(aegisPath, null, {
+      withCredentials: true,
+    });
+    clientAccounts.getAccountList(
+      requestAccount,
+      {
+        env: envFabric,
+      },
+      handleAccountsCallback(name)
+    );
+  }
 };
 
-getData(USERS.kevin);
+// getData(USERS.kevin);
 
 setTimeout(() => {
   document.getElementById("try-kevin").onclick = () => {
@@ -106,10 +168,35 @@ setTimeout(() => {
   document.getElementById("try-wendy").onclick = () => {
     getData(USERS.wendy);
   };
+  document.getElementById("try-kevin-direct").onclick = () => {
+    getData(USERS.kevin, true);
+  };
+  document.getElementById("try-wendy-direct").onclick = () => {
+    getData(USERS.wendy, true);
+  };
+  document.getElementById("try-kevin-account-direct").onclick = () => {
+    getAccounts(USERS.kevin, true);
+  };
+  document.getElementById("try-wendy-account-direct").onclick = () => {
+    getAccounts(USERS.wendy, true);
+  };
   document.getElementById("try-kevin-account").onclick = () => {
     getAccounts(USERS.kevin);
   };
   document.getElementById("try-wendy-account").onclick = () => {
     getAccounts(USERS.wendy);
   };
+  document.getElementById("env").onchange = (event) => {
+    updateAegisEnv(event.target.value);
+  };
+  document.getElementById("env-fabric").onchange = (event) => {
+    updateFabricEnv(event.target.value);
+  };
+  document.getElementById("token").onchange = (event) => {
+    document.cookie = `anzssotoken=${event.target.value}; path=/; domain=.gcpnp.anz; secure; samesite=lax`;
+    // document.cookie = `anzssotoken=${event.target.value}; path=/; samesite=lax`;
+  };
 }, 1000);
+
+const enableDevTools = window.__GRPCWEB_DEVTOOLS__ || (() => {});
+enableDevTools([client, clientDirect, clientAccounts, clientAccountsDirect]);
